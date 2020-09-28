7f045dda6defa3301a2b428a4589209a41da369a
Alper Donmez <alper.donmez490@gmail.com>
2020-09-28 18:58:35 -0400

    Create Refill Form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './App.css';
 import ProviderList from './ProviderList';
+import RefillForm from './RefillForm';
 
 class App extends React.Component {
 
   state = {
     selectedProvider: null,
     availableProviders: ["MTS", "Beeline", "Megafone"],
+    balances: {
+      "MTS": 0,
+      "Beeline": 12,
+      "Megafone": 0
+    }
   }
 
   handleSelectProvider = (provider) => {
     this.setState(() => ({ selectedProvider: provider }));
+  }
+
+  handleUpdateBalance = (refillAmount) => {
+    const newBalances = { ...this.state.balances };
+    newBalances[this.state.selectedProvider] += refillAmount;
+    this.setState(() => ({ balances: newBalances }));
   }
 
   render() {
@@ -25,7 +37,15 @@ class App extends React.Component {
               providers={this.state.availableProviders}
               handleSelectProvider={this.handleSelectProvider}
             />
-          ) : null}
+          ) : (
+              <RefillForm
+                selectedProvider={this.state.selectedProvider}
+                currentBalance={this.state.balances[this.state.selectedProvider]}
+                handleSelectProvider={this.handleSelectProvider}
+                handleUpdateBalance={this.handleUpdateBalance}
+              />
+
+            )}
 
       </div>
     );
---
+++ src/RefillForm.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import './RefillForm.css';
+
+class RefillForm extends React.Component {
+
+    state = {
+        refillAmount: '',
+        phoneNumber: '',
+        errorMessage: '',
+    }
+
+    handleAmountChange = (e) => {
+        const amount = e.target.value;
+        const reg = /^\d+(\.\d{0,2})?$/;
+        if (!amount || amount.match(reg)) {
+            this.setState(() => ({ refillAmount: amount }));
+        }
+    }
+
+    handlePhoneNumberChange = (e) => {
+        const amount = e.target.value;
+        const reg = /^\d+(\.\d{0,2})?$/;
+        if (!amount || amount.match(reg)) {
+            this.setState(() => ({ phoneNumber: amount }));
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const refillAmount = parseFloat(this.state.refillAmount);
+
+        if (isNaN(refillAmount) || refillAmount < 1 || refillAmount > 1000) {
+            this.setState(() => ({ errorMessage: "Please enter a value between 1 and 1000" }));
+        } else {
+            this.props.handleUpdateBalance(refillAmount);
+            this.setState(() => ({
+                refillAmount: '',
+                phoneNumber: '',
+                errorMessage: '',
+            }));
+        }
+    }
+
+    render() {
+        return (
+            <div className="refill-page">
+                <h2 className="selected-provider">{this.props.selectedProvider}</h2>
+                <p className="balance">Current balance: ${this.props.currentBalance}</p>
+                {this.state.errorMessage.length ? <p>{this.state.errorMessage}</p> : null}
+                <form className="refill-form">
+                    <input
+                        type="text"
+                        onChange={this.handleAmountChange}
+                        value={this.state.refillAmount}
+                        placeholder="Enter refill amount"
+                        className="number-input"
+                    />
+                    <input
+                        type="text"
+                        onChange={this.handlePhoneNumberChange}
+                        value={this.state.phoneNumber}
+                        placeholder="Enter refill phone number"
+                        className="number-input"
+                    />
+                    <button
+                        type="submit"
+                        onClick={this.handleSubmit}
+                        className="submit-button"
+                    >
+                        Submit
+                    </button>
+                </form>
+
+                <button onClick={() => this.props.handleSelectProvider(null)}>Return home</button>
+            </div>
+        );
+    }
+}
+
+export default RefillForm;
